5bfacb47dcd321471b330c13c025fab916631172
Ivan Klabučar <ivan.klabucar@fer.hr>
2021-11-10 17:16:33 +0100

    Most of coding finished, we need to clean up, make some graphs and make a report

--- assignment_1/functions.py
+++ assignment_1/functions.py
@@ -4,14 +4,13 @@ import zlib
 # document being a long string 
 def shingle(document, k=10):
     result = set()
-
-    words = document.split()
-    for i in range(len(words) - k + 1):
-        shingle = words[i:(i+k)]
-        #lists are mutable hence not hashable which is why we have to create a single string to hash
-        shingle_string = ''.join(shingle)
-        result.add(zlib.adler32(shingle_string.encode('utf8')))
-
+    # preparing the text
+    text = ''.join(document.split()).lower()
+    text = text.replace('!', '').replace(',', '').replace('.', '').replace('?', '').replace('"', '')
+    #generating the hashed shingles
+    for i in range(len(text) - k + 1):
+        shingle = text[i:(i+k)]
+        result.add(zlib.adler32(shingle.encode('utf8')))
     return result
 
 #computes the Jaccard similarity of two shingled documents
@@ -20,15 +19,19 @@ def compareSets(set1: set, set2: set):
     intersection = set.intersection(set1, set2)
     return len(intersection) / len(union)
 
+def compare_signatures(sig1, sig2):
+    equal = 0
+    assert len(sig1) == len(sig2)
+    for s1, s2 in zip(sig1, sig2):
+        equal += 1 if s1 == s2 else 0
+    return equal / len(sig1)
+
 class MinHash:
     def __init__(self, n=100) -> None:
         self.n_hash_functions = n
         self.c = 2**32 - 1
-        self.a = list()
-        self.b = list()
-        for _ in range(n):
-            self.a.append(random.randint(0, self.c))
-            self.b.append(random.randint(0, self.c))
+        self.a = random.sample(range(0, self.c), n)
+        self.b = random.sample(range(0, self.c), n)
         self.hash_function = lambda x, i: (self.a[i]*x + self.b[i]) % self.c
 
     def create_signature(self, hashes):
@@ -40,10 +43,4 @@ class MinHash:
             signature.append(s)
         return signature
 
-
-    def compareSets(self, sig1, sig2):
-        equal = 0
-        for s1, s2 in zip(sig1, sig2):
-            equal += 1 if s1 == s2 else 0
-        return equal / self.n_hash_functions
 
