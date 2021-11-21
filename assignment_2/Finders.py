f04e26d94da160c475cc8bb3ce239e766a9595df
Simon Langrieger <simon.langrieger@tum.de>
2021-11-21 18:22:29 +0100

    some commenting and run the code with multiple support values

--- assignment_2/Finders.py
+++ assignment_2/Finders.py
@@ -38,26 +38,29 @@ class FrequentItemsetFinder:
         self.N = len(self.dataset)
 
     # The first step is to find frequent items, which is different from other steps because we have to look at all items
-    def find_freq_items(self):      # There's no duplicates in baskets I checked.
+    def find_freq_items(self):
         cnt = dict()
         for basket in self.dataset:
             for item in basket:
                 if item not in cnt: cnt[item] = 0
                 cnt[item] += 1
 
-        self.freq_itemsets[1] = {(item,) for item in cnt if cnt[item] >= (self.s * self.N)}  # Keep only items with relative support s
+        # Keep only items with relative support s
+        self.freq_itemsets[1] = {(item,) for item in cnt if cnt[item] >= (self.s * self.N)}
         return len(self.freq_itemsets[1]) > 0
     
-    def check_if_viable_combination(self, itemset, item):          # Check if it makes sense to expand an itemset with certain item
+    # Check if it makes sense to expand an itemset with certain item
+    def check_if_viable_combination(self, itemset, item):          
         k = len(itemset)
-        if item[0] in itemset: return False
-        for m in range(1, k):                        # There's no point in expanding an itemset with an element already present
-            for subset in itertools.combinations(itemset, m):  # All subsets of the original itemset
-                combination = tuple(sorted(list(subset + item)))              
-                if combination not in self.freq_itemsets[m+1]: return False     # Because all subsets have at least the same support as their super sets
-        return True                                                        # this conditions gets rid of all combinations which we know can't be frequent because we have concluded that one of its subsets is unfrequent in the previous pass
+        if item[0] in itemset: return False     # There's no point in expanding an itemset with an element already present
+        for m in range(1, k):       # All subsets of the original itemset
+            for subset in itertools.combinations(itemset, m):  # Because all subsets have at least the same support as their super sets
+                combination = tuple(sorted(list(subset + item)))   # this conditions gets rid of all combinations which we know can't be frequent because we have concluded that one of its subsets is unfrequent in the previous pass 
+                if combination not in self.freq_itemsets[m+1]: return False     
+        return True                                                        
     
-    def generate_candidates(self, k):                                     # Generate candidate itemsets which might be frequent (all of their subsets are frequent)
+    # Generate candidate itemsets which might be frequent (all of their subsets are frequent)
+    def generate_candidates(self, k):
         candidates = set()
         for item in self.freq_itemsets[1]:
             for itemset in self.freq_itemsets[k - 1]:
@@ -65,7 +68,8 @@ class FrequentItemsetFinder:
                     candidates.add(tuple(sorted(list(itemset + item))))
         return candidates
     
-    def only_freq_itemsets(self, candidates):                             # Keep only the candidates that have support at least s * size of dataset
+    # Keep only the candidates that have support at least s * size of dataset
+    def only_freq_itemsets(self, candidates):
         cnt = dict((x, 0) for x in candidates)
 
         # we had big performance issues, therefore we use multiprocessing to speed everything up
@@ -75,21 +79,22 @@ class FrequentItemsetFinder:
         thresh = self.s * self.N
         return {c[0] for c in counter if c[1] >= thresh}
         
-
-    def find_k_itemsets(self, k):                                 # Find all frequent itemsets of size k
+    # Find all frequent itemsets of size k
+    def find_k_itemsets(self, k):
         if k < 1: raise Exception('k must be greater than 0.')
         if k == 1: return self.find_freq_items()   # As in itemsets of size 1
         
         candidates = self.generate_candidates(k)
         self.freq_itemsets[k] = self.only_freq_itemsets(candidates)
 
-        return len(self.freq_itemsets[k]) > 0                    # If none such itemsets exist return False
+        return len(self.freq_itemsets[k]) > 0  # If none such itemsets exist return False
     
-    def find_all_frequent_itemsets(self):       # starting at k=1 find all frequent k-itemsets
+    # starting at k=1 find all frequent k-itemsets
+    def find_all_frequent_itemsets(self):
         k = 0
         done = False
         elapsed_time = 0
-        while not done:                         # Stop searching when there are no such itemsets for the current k
+        while not done:  # Stop searching when there are no such itemsets for the current k
             k += 1
             start = time.time()
             done = not self.find_k_itemsets(k)
@@ -99,6 +104,7 @@ class FrequentItemsetFinder:
         print(f'[FIFinder]: Found all frequent itemsets with support threshold {self.s}% in {elapsed_time}s.')
         return k - 1, elapsed_time     # Return last frequent k-itemsets found
 
+    # returns the frequent itemsets together with the absolute count
     def getFreqItemsetsWithSupport(self):
         counts = dict()
         for itemsets in self.freq_itemsets.values():
